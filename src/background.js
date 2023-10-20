a8fc51859f3417deb4a8bdbbcbd3d7603d16e273
meleshun <meleshun@gmail.com>
2023-10-20 18:05:23 +0200

    initial commit

---
+++ src/background.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// With background scripts you can communicate with popup
+// and contentScript files.
+// For more information on background script,
+// See https://developer.chrome.com/extensions/background_pages
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'setBadgeText') {
+    chrome.history.search(
+      { text: `"${getHostname(sender.tab.url)}"`, maxResults: 0 },
+      (historyItems) => {
+        //   const totalVisitCount = historyItems?.reduce(
+        //     (total, item) => total + item.visitCount,
+        //     0
+        //   );
+        console.log(historyItems);
+        const totalVisitCount = historyItems.length;
+        chrome.action.setBadgeText({
+          text: String(totalVisitCount),
+          tabId: sender.tab.id,
+        });
+      }
+    );
+  }
+});
+
+chrome.action.onClicked.addListener((tab) => {
+  openHistory(getHostname(tab.url));
+});
+
+const openHistory = (query) => {
+  chrome.tabs.create({ url: `chrome://history/?q="${query}"` });
+};
+
+const getHostname = (url) => {
+  const currentUrl = new URL(url);
+  return currentUrl.hostname;
+};
---
+++ src/contentScript.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Content script file will run in the context of web page.
+// With content script you can manipulate the web pages using
+// Document Object Model (DOM).
+// You can also pass information to the parent extension.
+
+// We execute this script by making an entry in manifest.json file
+// under `content_scripts` property
+
+// For more information on Content Scripts,
+// See https://developer.chrome.com/extensions/content_scripts
+
+chrome.runtime.sendMessage({ action: 'setBadgeText' });
